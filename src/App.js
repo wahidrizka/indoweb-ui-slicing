0a67f852bd3b926745e2371ebbb5cd41910a4a19
wahidrizka <wahid.0310@students.amikom.ac.id>
2023-03-01 20:27:20 +0700

    🚀: initial commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./App.css";
+function App() {
+	return (
+		<>
+			<header className="header_area header_stick">
+				<nav className="navbar navbar-expand-lg">
+					<div className="container">
+						<a href="#home" className="navbar-brand">
+							<img
+								src="https://indoweb.id/wp-content/uploads/2022/12/1.png"
+								alt="Indoweb"
+								srcSet="https://indoweb.id/wp-content/uploads/2022/12/1.png 2x"
+							/>
+						</a>
+						<div className="navbar-collapse justify-content-center">
+							<ul className="navbar-nav menu">
+								<li className="menu-item">
+									<a href="#" className="nav-link">
+										Beranda
+									</a>
+								</li>
+								<li className="menu-item">
+									<a href="#" className="nav-link">
+										Tentang
+									</a>
+								</li>
+								<li className="menu-item">
+									<a href="#" className="nav-link">
+										Harga
+									</a>
+								</li>
+								<li className="menu-item">
+									<a href="#" className="nav-link">
+										Blog
+									</a>
+								</li>
+								<li className="menu-item">
+									<a href="#" className="nav-link">
+										Kontak
+									</a>
+								</li>
+							</ul>
+						</div>
+						<a href="#" className="btn_get">
+							Daftar
+						</a>
+					</div>
+				</nav>
+			</header>
+
+			<section className="elementor-section">
+				<div className="elementor-container elementor-column-gap-default">
+					<div className="elementor-column elementor-col-50 elementor-top-column elementor-element">
+						<div className="elementor-widget-wrap elementor-element-populated">
+							<div className="elementor-element elementor-element-4ef1d02 elementor-widget elementor-widget-image">
+								<div className="elementor-widget-container">
+									<img
+										decoding="async"
+										width="1200"
+										height="750"
+										src="https://indoweb.id/wp-content/uploads/2023/02/055.jpg"
+										className="attachment-full size-full wp-image-14563"
+										alt=""
+										loading="lazy"
+										srcset="https://indoweb.id/wp-content/uploads/2023/02/055.jpg 1200w, https://indoweb.id/wp-content/uploads/2023/02/055-300x188.jpg 300w, https://indoweb.id/wp-content/uploads/2023/02/055-1024x640.jpg 1024w, https://indoweb.id/wp-content/uploads/2023/02/055-768x480.jpg 768w, https://indoweb.id/wp-content/uploads/2023/02/055-770x480.jpg 770w"
+										sizes="(max-width: 1200px) 100vw, 1200px"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="elementor-column elementor-col-50 elementor-top-column elementor-element">
+						<div className="elementor-widget-wrap elementor-element-populated">
+							<div className="elementor-element elementor-element-ed90844 elementor-widget elementor-widget-heading">
+								<div className="elementor-widget-container">
+									<h2 className="elementor-heading-title">
+										Indoweb.id - Ahlinya Website, Android &amp; Aplikasi
+									</h2>
+								</div>
+							</div>
+							<div className="elementor-element elementor-element-b7012a0 elementor-widget elementor-widget-text-editor">
+								<div className="elementor-widget-container">
+									<p>
+										<strong>Indoweb.id</strong>&nbsp;adalah perusahan yang
+										bergerak dalam bidang pembuatan website, toko online serta
+										software development. Pusat Kantor berada di kediri jawa
+										timur indonesia. Berdiri sejak 2007 dan menghasilkan banyak
+										produk mulai dari website Pendidikan, Perusahaan, Personal,
+										Universitas maupun aplikasi web seperti SIM Kampus,HRMS,
+										Keuangan serta aplikasi untuk perusahaan. Website mempunyai
+										banyak fungsi dari promosi produk sampai transaksi, maka
+										dari itu sebuah website dengan tampilan yang bagus serta
+										tingkat keamanan yang tinggi sangat diperlukan bagi Anda
+										yang ingin memanfaatkan teknologi internet. Kami dengan
+										senang hati akan membantu anda untuk itu.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default App;
